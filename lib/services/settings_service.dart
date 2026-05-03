7792c4e35d851edec7176ce4f16f9375edca13df
hanif4343 <hsarder43@gmail.com>
2026-05-03 11:13:46 +0600

    new-p1: update lib/services/settings_service.dart

--- lib/services/settings_service.dart
+++ lib/services/settings_service.dart
@@ -12,19 +12,6 @@ class SettingsService {
     return _prefs!;
   }
 
-  // Theme
   static bool get isDark => _p.getBool('isDark') ?? true;
   static Future<void> setDark(bool val) => _p.setBool('isDark', val);
-
-  // PIN
-  static bool get pinEnabled => _p.getBool('pinEnabled') ?? false;
-  static String get pin => _p.getString('pin') ?? '';
-  static Future<void> setPin(String val) async {
-    await _p.setString('pin', val);
-    await _p.setBool('pinEnabled', val.isNotEmpty);
-  }
-  static Future<void> disablePin() async {
-    await _p.setBool('pinEnabled', false);
-    await _p.setString('pin', '');
-  }
 }
